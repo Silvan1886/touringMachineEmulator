b8881176765cfa45fecd87d06d49f8e85826daa6
Studer Silvan (stusil01) <stusil01@students.zhaw.ch>
2025-05-01 14:39:11 +0200

    made public

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿
 START_STATE = "q1";
 ACCEPTING_STATE = "q2";
-record Transition(string State, char ReadSymbol, string NextState, char WriteSymbol, bool Direction);
+public record Transition(string State, char ReadSymbol, string NextState, char WriteSymbol, bool Direction);
