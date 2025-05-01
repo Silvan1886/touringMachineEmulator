56ec65555c752c87e8949baac1a61a06dec02905
Studer Silvan (stusil01) <stusil01@students.zhaw.ch>
2025-05-01 14:38:49 +0200

    some changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+START_STATE = "q1";
+ACCEPTING_STATE = "q2";
+record Transition(string State, char ReadSymbol, string NextState, char WriteSymbol, bool Direction);
